9582384872f7f202495ec1f03748d6ca9acaccdc
GabrielLerner <glerner@renttherunway.com>
2018-09-02 18:42:01 -0400

    For gcloud datapoint generation

---
+++ datapoint_generator.py
@@ -0,0 +1,12 @@
+from main import RFest
+import pandas as pd
+import time
+
+generator=RFest()
+generator.drop_rate=0.99
+generator.rows_range=[100000, 1000000, 10000000]
+
+result=generator.generate_data()
+
+result[0].to_csv('input'+str(time.time())+'.csv')
+result[1].to_csv('output'+str(time.time())+'.csv')
