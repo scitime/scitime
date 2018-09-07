350b9d021acf68c26a0b94c07aadf24583c6d5ee
Gabriel Lerner <glerner@renttherunway.com>
2018-09-06 20:36:48 -0400

    changing inputs

--- datapoint_generator.py
+++ datapoint_generator.py
@@ -4,9 +4,9 @@ import time
 
 generator=RFest()
 generator.drop_rate=0.999
-generator.rows_range=[100000, 1000000, 10000000, 25000000, 50000000]
+generator.rows_range=[25000000, 50000000,100000000]
 generator.max_depth_range=[10,50,100,200]
-generator.inputs_range=[10,50,100,1000]
-generator.max_features_range=['auto'  ,10,20,50]
+generator.inputs_range=[200,500,700,1000]
+generator.max_features_range=['auto'  20,50,100]
 
 generator.generate_data()
