00ffe077630de9a3f34c233daa2c99bf0c3d6bee
GabrielLerner <glerner@renttherunway.com>
2018-09-06 21:34:49 -0400

    reducing size

--- datapoint_generator.py
+++ datapoint_generator.py
@@ -6,7 +6,7 @@ generator=RFest()
 generator.drop_rate=0.999
 generator.rows_range=[500000, 1000000,10000000]
 generator.max_depth_range=[10,50,100,200]
-generator.inputs_range=[200,500,700,1000]
+generator.inputs_range=[200,300,500,600]
 generator.max_features_range=['auto' , 20,50,100]
 
 generator.generate_data()
