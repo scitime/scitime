1c6973dcc9c7b2c9c308ea8b5f8e71df25982162
GabrielLerner <glerner@renttherunway.com>
2018-09-06 20:39:39 -0400

    oops

--- datapoint_generator.py
+++ datapoint_generator.py
@@ -7,6 +7,6 @@ generator.drop_rate=0.999
 generator.rows_range=[25000000, 50000000,100000000]
 generator.max_depth_range=[10,50,100,200]
 generator.inputs_range=[200,500,700,1000]
-generator.max_features_range=['auto'  20,50,100]
+generator.max_features_range=['auto' , 20,50,100]
 
 generator.generate_data()
