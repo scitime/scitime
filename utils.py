4ddadc23b488e91283d1c53476eb0026899b6d14
Nathan <toubiana.nathan@gmail.com>
2018-10-06 12:26:49 -0400

    [WIP] config.json + get_path util

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import logging
 import csv
+import json
+from os import path
 
 def Logging(__name__):
     """
@@ -25,3 +27,19 @@ def add_data_to_csv(thisInput, thisOutput, parameters_list):
         writer = csv.writer(file)
         thisRow = list(thisInput) + [thisOutput]
         writer.writerows([thisRow])
+
+def get_path(file):
+    """
+    returns current path
+    """
+
+    return path.join(path.dirname(path.abspath(__file__)), file)
+
+def config(key):
+    """
+    loads json data
+
+    :param key: specific key to load
+    :return: dictionary
+    """
+    return json.load(open(get_path('config.json')))[key]
