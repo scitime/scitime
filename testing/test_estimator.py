2b3303c4582f8734f917199b7a5b58566a10ed24
Nathan <toubiana.nathan@gmail.com>
2018-10-31 19:18:38 -0400

    testing framework for estimate [WIP]

--- testing/test_estimator.py
+++ testing/test_estimator.py
@@ -13,23 +13,13 @@ class TestEstimate(unittest.TestCase):
         self.estimator = Estimator(meta_algo='RF', verbose=0)
 
     def test_fetch_name(self):
-        rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=200,
-                                   max_features=10, max_leaf_nodes=10, min_impurity_decrease=10,
-                                   min_impurity_split=10, min_samples_leaf=10,
-                                   min_samples_split=10, min_weight_fraction_leaf=0.5,
-                                   n_estimators=100, n_jobs=10, oob_score=False, random_state=None,
-                                   verbose=2, warm_start=False)
+        rf = RandomForestRegressor()
         name = self.estimator._fetch_name(rf)
         assert name == 'RandomForestRegressor'
 
 
     def test_estimate_duration(self):
-        rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=200,
-                                   max_features=10, max_leaf_nodes=10, min_impurity_decrease=10,
-                                   min_impurity_split=10, min_samples_leaf=10,
-                                   min_samples_split=10, min_weight_fraction_leaf=0.5,
-                                   n_estimators=100, n_jobs=10, oob_score=False, random_state=None,
-                                   verbose=2, warm_start=False)
+        rf = RandomForestRegressor()
         X, y = np.random.rand(100000, 10), np.random.rand(100000, 1)
         # run the estimation
         duration = self.estimator.estimate_duration(rf, X, y)
