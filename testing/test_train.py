6ed7f8dd085f8d503b7b3582fff3617b62c9fe7d
Nathan <toubiana.nathan@gmail.com>
2018-11-01 00:05:05 -0400

    refing tests

--- testing/test_train.py
+++ testing/test_train.py
@@ -7,27 +7,36 @@ class TestTrain(unittest.TestCase):
     inputs, outputs = None, None
 
     def setUp(self):
-        self.rf_trainer = Trainer(drop_rate=0.99999, verbose=0, algo='RandomForestRegressor')
-        self.svc_trainer = Trainer(drop_rate=0.999999, verbose=0, algo='SVC')
-        self.km_trainer = Trainer(drop_rate=0.99, verbose=0, algo='KMeans')
+        self.rf_trainer = Trainer(drop_rate=0.999995, verbose=0, algo='RandomForestRegressor')
+        self.svc_trainer = Trainer(drop_rate=0.999998, verbose=0, algo='SVC')
+        self.km_trainer = Trainer(drop_rate=0.993, verbose=0, algo='KMeans')
 
-    def test_generate_data(self):
+    def test_generate_data_rf(self):
         rf_inputs, rf_outputs = self.rf_trainer._generate_data()
-        svc_inputs, svc_outputs = self.svc_trainer._generate_data()
-        km_inputs, km_outputs = self.km_trainer._generate_data()
+
         TestTrain.rf_inputs = rf_inputs
         TestTrain.rf_outputs = rf_outputs
-        TestTrain.svc_inputs = svc_inputs
-        TestTrain.svc_outputs = svc_outputs
-        TestTrain.km_inputs = km_inputs
-        TestTrain.km_outputs = km_outputs
-        assert svc_inputs.shape[0] > 0
-        assert svc_outputs.shape[0] > 0
+
         assert rf_inputs.shape[0] > 0
         assert rf_outputs.shape[0] > 0
+
+    def test_generate_data_svc(self):
+        svc_inputs, svc_outputs = self.svc_trainer._generate_data()
+
+        TestTrain.svc_inputs = svc_inputs
+        TestTrain.svc_outputs = svc_outputs
+
+        assert svc_inputs.shape[0] > 0
+        assert svc_outputs.shape[0] > 0
+
+    def test_generate_data_km(self):
+        km_inputs, km_outputs = self.km_trainer._generate_data()
+
+        TestTrain.km_inputs = km_inputs
+        TestTrain.km_outputs = km_outputs
+
         assert km_inputs.shape[0] > 0
         assert km_outputs.shape[0] > 0
-
 
     def test_model_fit(self):
         rf_meta_algo = self.rf_trainer.model_fit(generate_data=False, df=TestTrain.rf_inputs,
@@ -35,7 +44,7 @@ class TestTrain(unittest.TestCase):
         svc_meta_algo = self.svc_trainer.model_fit(generate_data=False, df=TestTrain.svc_inputs,
                                                    outputs=TestTrain.svc_outputs)
         km_meta_algo = self.km_trainer.model_fit(generate_data=False, df=TestTrain.km_inputs,
-                                                   outputs=TestTrain.km_outputs)
+                                                 outputs=TestTrain.km_outputs)
         assert type(rf_meta_algo).__name__ == 'RandomForestRegressor'
         assert type(svc_meta_algo).__name__ == 'RandomForestRegressor'
         assert type(km_meta_algo).__name__ == 'RandomForestRegressor'
