f1d80c9d280d828687f0e3a21f5054530df745a6
Nathan <toubiana.nathan@gmail.com>
2018-10-31 22:40:38 -0400

    more tests

--- testing/test_estimator.py
+++ testing/test_estimator.py
@@ -2,6 +2,8 @@ import unittest
 
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.cluster import KMeans
+from sklearn.svm import SVC
 
 from scikest.estimate import Estimator
 
@@ -16,17 +18,28 @@ class TestEstimate(unittest.TestCase):
         rf = RandomForestRegressor()
         svc = SVC()
         kmeans = KMeans()
-        name = self.estimator._fetch_name(rf)
-        assert name == 'RandomForestRegressor'
+        rf_name = self.estimator._fetch_name(rf)
+        svc_name = self.estimator._fetch_name(svc)
+        kmeans_name = self.estimator._fetch_name(kmeans)
+        assert rf_name == 'RandomForestRegressor'
+        assert svc_name == 'SVC'
+        assert kmeans_name == 'KMeans'
 
 
     def test_estimate_duration(self):
         rf = RandomForestRegressor()
-        X, y = np.random.rand(100000, 10), np.random.rand(100000, 1)
+        svc = SVC()
+        kmeans = KMeans()
+        X, y_continuous = np.random.rand(10000, 10), np.random.rand(10000, 1)
+        y_class = np.random.randint(0, 4, 10000)
         # run the estimation
-        duration = self.estimator.estimate_duration(rf, X, y)
+        rf_duration = self.estimator.estimate_duration(rf, X, y_continuous)
+        svc_duration = self.estimator.estimate_duration(svc, X, y_class)
+        kmeans_duration = self.estimator.estimate_duration(kmeans, X)
 
-        assert type(duration[0]) == np.float64
+        assert type(rf_duration[0]) == np.float64
+        assert type(svc_duration[0]) == np.float64
+        assert type(kmeans_duration[0]) == np.float64
 
 
 if __name__ == '__main__':
