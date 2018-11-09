63a803adea85938765fa3e9639dd26731dc9eb13
Philippe Mizrahi <philippe.mizrahi@polytechnique.org>
2018-11-09 14:48:10 -0500

    modified the tests

--- testing/test_estimator.py
+++ testing/test_estimator.py
@@ -31,18 +31,31 @@ class TestEstimate(unittest.TestCase):
         X, y_continuous = np.random.rand(10000, 10), np.random.rand(10000, 1)
         rf_duration = self.estimator.estimate_duration(rf, X, y_continuous)
         assert type(rf_duration[0]) == np.float64
+        assert type(rf_duration[1]) == np.float64
+        assert type(rf_duration[2]) == np.float64
+        assert rf_duration[1] < rf_duration[0]
+        assert rf_duration[0] < rf_duration[2]
+
 
     def test_estimate_duration_classification(self):
         svc = SVC()
         X, y_class = np.random.rand(10000, 10), np.random.randint(0, 4, 10000)
         svc_duration = self.estimator.estimate_duration(svc, X, y_class)
         assert type(svc_duration[0]) == np.float64
+        assert type(svc_duration[1]) == np.float64
+        assert type(svc_duration[2]) == np.float64
+        assert svc_duration[1] < svc_duration[0]
+        assert svc_duration[0] < svc_duration[2]
 
     def test_estimate_duration_unsupervised(self):
         kmeans = KMeans()
         X = np.random.rand(10000, 10)
         kmeans_duration = self.estimator.estimate_duration(kmeans, X)
         assert type(kmeans_duration[0]) == np.float64
+        assert type(kmeans_duration[1]) == np.float64
+        assert type(kmeans_duration[2]) == np.float64
+        assert kmeans_duration[1] < kmeans_duration[0]
+        assert kmeans_duration[0] < kmeans_duration[2]
 
 
 if __name__ == '__main__':
