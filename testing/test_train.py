9e3d53d9fb022a3aae4e9b771445847b2656e99b
Nathan <toubiana.nathan@gmail.com>
2018-12-02 21:42:41 -0500

    new tests

--- testing/test_train.py
+++ testing/test_train.py
@@ -10,12 +10,16 @@ class TestTrain(unittest.TestCase):
     inputs, outputs = None, None
 
     def setUp(self):
-        self.rf_trainer = Trainer(drop_rate=1, verbose=3, algo='RandomForestRegressor')
-        self.svc_trainer = Trainer(drop_rate=1, verbose=3, algo='SVC')
-        self.km_trainer = Trainer(drop_rate=1, verbose=3, algo='KMeans')
+        self.rf_trainer_metarf = Trainer(drop_rate=1, verbose=3, algo='RandomForestRegressor', meta_algo='RF')
+        self.svc_trainer_metarf = Trainer(drop_rate=1, verbose=3, algo='SVC', meta_algo='RF')
+        self.km_trainer_metarf = Trainer(drop_rate=1, verbose=3, algo='KMeans', meta_algo='RF')
 
-    def test_generate_data_regression(self):
-        rf_inputs, rf_outputs, _ = self.rf_trainer._generate_data()
+        self.rf_trainer_metann = Trainer(drop_rate=1, verbose=3, algo='RandomForestRegressor', meta_algo='NN')
+        self.svc_trainer_metann = Trainer(drop_rate=1, verbose=3, algo='SVC', meta_algo='NN')
+        self.km_trainer_metann = Trainer(drop_rate=1, verbose=3, algo='KMeans', meta_algo='NN')
+
+    def test_generate_data_regression_metarf(self):
+        rf_inputs, rf_outputs, _ = self.rf_trainer_metarf._generate_data()
 
         TestTrain.rf_inputs = rf_inputs
         TestTrain.rf_outputs = rf_outputs
@@ -23,8 +27,8 @@ class TestTrain(unittest.TestCase):
         assert rf_inputs.shape[0] > 0
         assert rf_outputs.shape[0] == rf_inputs.shape[0]
 
-    def test_generate_data_classification(self):
-        svc_inputs, svc_outputs, _ = self.svc_trainer._generate_data()
+    def test_generate_data_classification_metarf(self):
+        svc_inputs, svc_outputs, _ = self.svc_trainer_metarf._generate_data()
 
         TestTrain.svc_inputs = svc_inputs
         TestTrain.svc_outputs = svc_outputs
@@ -32,8 +36,8 @@ class TestTrain(unittest.TestCase):
         assert svc_inputs.shape[0] > 0
         assert svc_outputs.shape[0] == svc_inputs.shape[0]
 
-    def test_generate_data_unsupervised(self):
-        km_inputs, km_outputs, _ = self.km_trainer._generate_data()
+    def test_generate_data_unsupervised_metarf(self):
+        km_inputs, km_outputs, _ = self.km_trainer_metarf._generate_data()
 
         TestTrain.km_inputs = km_inputs
         TestTrain.km_outputs = km_outputs
@@ -41,52 +45,110 @@ class TestTrain(unittest.TestCase):
         assert km_inputs.shape[0] > 0
         assert km_outputs.shape[0] == km_inputs.shape[0]
 
-    def test_validate_data_regression(self):
-        rf_val_inputs, rf_val_outputs, rf_val_estimated_outputs, rf_val_avg_weighted_error = self.rf_trainer.model_validate()
+    def test_validate_data_regression_metarf(self):
+        rf_val_inputs, rf_val_outputs, rf_val_estimated_outputs, rf_val_avg_weighted_error = self.rf_trainer_metarf.model_validate()
 
         assert rf_val_inputs.shape[0] > 0
         assert rf_val_outputs.shape[0] == rf_val_inputs.shape[0]
         assert rf_val_estimated_outputs.shape[0] == rf_val_inputs.shape[0]
         assert type(rf_val_avg_weighted_error) == np.float64
 
-    def test_validate_data_classification(self):
-        svc_val_inputs, svc_val_outputs, svc_val_estimated_outputs, svc_val_avg_weighted_error = self.svc_trainer.model_validate()
+    def test_validate_data_classification_metarf(self):
+        svc_val_inputs, svc_val_outputs, svc_val_estimated_outputs, svc_val_avg_weighted_error = self.svc_trainer_metarf.model_validate()
 
         assert svc_val_inputs.shape[0] > 0
         assert svc_val_outputs.shape[0] == svc_val_inputs.shape[0]
         assert svc_val_estimated_outputs.shape[0] == svc_val_inputs.shape[0]
         assert type(svc_val_avg_weighted_error) == np.float64
 
-    def test_validate_data_classification(self):
-        svc_val_inputs, svc_val_outputs, svc_val_estimated_outputs, svc_val_avg_weighted_error = self.svc_trainer.model_validate()
-
-        assert svc_val_inputs.shape[0] > 0
-        assert svc_val_outputs.shape[0] == svc_val_inputs.shape[0]
-        assert svc_val_estimated_outputs.shape[0] == svc_val_inputs.shape[0]
-        assert type(svc_val_avg_weighted_error) == np.float64
-
-    def test_validate_data_unsupervised(self):
-        km_val_inputs, km_val_outputs, km_val_estimated_outputs, km_val_avg_weighted_error = self.km_trainer.model_validate()
+    def test_validate_data_unsupervised_metarf(self):
+        km_val_inputs, km_val_outputs, km_val_estimated_outputs, km_val_avg_weighted_error = self.km_trainer_metarf.model_validate()
 
         assert km_val_inputs.shape[0] > 0
         assert km_val_outputs.shape[0] == km_val_inputs.shape[0]
         assert km_val_estimated_outputs.shape[0] == km_val_inputs.shape[0]
         assert type(km_val_avg_weighted_error) == np.float64
 
-    def test_model_fit_regression(self):
-        rf_meta_algo = self.rf_trainer.model_fit(generate_data=False, inputs=TestTrain.rf_inputs,
+    def test_model_fit_regression_metarf(self):
+        rf_meta_algo = self.rf_trainer_metarf.model_fit(generate_data=False, inputs=TestTrain.rf_inputs,
                                                  outputs=TestTrain.rf_outputs)
         assert type(rf_meta_algo).__name__ == 'RandomForestRegressor'
 
-    def test_model_fit_classification(self):
-        svc_meta_algo = self.svc_trainer.model_fit(generate_data=False, inputs=TestTrain.svc_inputs,
+    def test_model_fit_classification_metarf(self):
+        svc_meta_algo = self.svc_trainer_metarf.model_fit(generate_data=False, inputs=TestTrain.svc_inputs,
                                                    outputs=TestTrain.svc_outputs)
         assert type(svc_meta_algo).__name__ == 'RandomForestRegressor'
 
-    def test_model_fit_unsupervised(self):
-        km_meta_algo = self.km_trainer.model_fit(generate_data=False, inputs=TestTrain.km_inputs,
+    def test_model_fit_unsupervised_metarf(self):
+        km_meta_algo = self.km_trainer_metarf.model_fit(generate_data=False, inputs=TestTrain.km_inputs,
                                                  outputs=TestTrain.km_outputs)
         assert type(km_meta_algo).__name__ == 'RandomForestRegressor'
+
+    def test_generate_data_regression_metann(self):
+        rf_inputs, rf_outputs, _ = self.rf_trainer_metann._generate_data()
+
+        TestTrain.rf_inputs = rf_inputs
+        TestTrain.rf_outputs = rf_outputs
+
+        assert rf_inputs.shape[0] > 0
+        assert rf_outputs.shape[0] == rf_inputs.shape[0]
+
+    def test_generate_data_classification_metann(self):
+        svc_inputs, svc_outputs, _ = self.svc_trainer_metann._generate_data()
+
+        TestTrain.svc_inputs = svc_inputs
+        TestTrain.svc_outputs = svc_outputs
+
+        assert svc_inputs.shape[0] > 0
+        assert svc_outputs.shape[0] == svc_inputs.shape[0]
+
+    def test_generate_data_unsupervised_metann(self):
+        km_inputs, km_outputs, _ = self.km_trainer_metann._generate_data()
+
+        TestTrain.km_inputs = km_inputs
+        TestTrain.km_outputs = km_outputs
+
+        assert km_inputs.shape[0] > 0
+        assert km_outputs.shape[0] == km_inputs.shape[0]
+
+    def test_validate_data_regression_metann(self):
+        rf_val_inputs, rf_val_outputs, rf_val_estimated_outputs, rf_val_avg_weighted_error = self.rf_trainer_metann.model_validate()
+
+        assert rf_val_inputs.shape[0] > 0
+        assert rf_val_outputs.shape[0] == rf_val_inputs.shape[0]
+        assert rf_val_estimated_outputs.shape[0] == rf_val_inputs.shape[0]
+        assert type(rf_val_avg_weighted_error) == np.float64
+
+    def test_validate_data_classification_metann(self):
+        svc_val_inputs, svc_val_outputs, svc_val_estimated_outputs, svc_val_avg_weighted_error = self.svc_trainer_metann.model_validate()
+
+        assert svc_val_inputs.shape[0] > 0
+        assert svc_val_outputs.shape[0] == svc_val_inputs.shape[0]
+        assert svc_val_estimated_outputs.shape[0] == svc_val_inputs.shape[0]
+        assert type(svc_val_avg_weighted_error) == np.float64
+
+    def test_validate_data_unsupervised_metann(self):
+        km_val_inputs, km_val_outputs, km_val_estimated_outputs, km_val_avg_weighted_error = self.km_trainer_metann.model_validate()
+
+        assert km_val_inputs.shape[0] > 0
+        assert km_val_outputs.shape[0] == km_val_inputs.shape[0]
+        assert km_val_estimated_outputs.shape[0] == km_val_inputs.shape[0]
+        assert type(km_val_avg_weighted_error) == np.float64
+
+    def test_model_fit_regression_metann(self):
+        rf_meta_algo = self.rf_trainer_metann.model_fit(generate_data=False, inputs=TestTrain.rf_inputs,
+                                                 outputs=TestTrain.rf_outputs)
+        assert type(rf_meta_algo).__name__ == 'MLPRegressor'
+
+    def test_model_fit_classification_metann(self):
+        svc_meta_algo = self.svc_trainer_metann.model_fit(generate_data=False, inputs=TestTrain.svc_inputs,
+                                                   outputs=TestTrain.svc_outputs)
+        assert type(svc_meta_algo).__name__ == 'MLPRegressor'
+
+    def test_model_fit_unsupervised_metann(self):
+        km_meta_algo = self.km_trainer_metann.model_fit(generate_data=False, inputs=TestTrain.km_inputs,
+                                                 outputs=TestTrain.km_outputs)
+        assert type(km_meta_algo).__name__ == 'MLPRegressor'
 
 
 if __name__ == '__main__':
