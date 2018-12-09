3beb70f3e4635e21b23839f62e052ffd9e11c76d
Gabriel Lerner <glerner@renttherunway.com>
2018-12-09 16:34:08 -0500

    param space in Json

--- scikest/train.py
+++ scikest/train.py
@@ -34,22 +34,13 @@ class Trainer(Estimator, LogMixin):
     DROP_RATE = 0.9
     # the default estimated algorithm is a Random Forest from sklearn
     ALGO = 'RandomForestRegressor'
-    #Parameters for Random Search
-    PARAMETER_SPACE = {
-    'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
-    'activation': ['tanh', 'relu'],
-    'solver': ['sgd', 'adam'],
-    'alpha': [0.0001, 0.05],
-    'learning_rate': ['constant','adaptive'],
-    }
 
-    def __init__(self, drop_rate=DROP_RATE, meta_algo=META_ALGO, algo=ALGO, parameter_space=PARAMETER_SPACE, verbose=0):
+    def __init__(self, drop_rate=DROP_RATE, meta_algo=META_ALGO, algo=ALGO, verbose=0):
         # the end user will estimate the fitting time of self.algo using the package
         self.algo = algo
         self.drop_rate = drop_rate
         self.meta_algo = meta_algo
         self.verbose = verbose
-        self.parameter_space = parameter_space
 
     @property
     def num_cpu(self):
@@ -287,11 +278,12 @@ class Trainer(Estimator, LogMixin):
         if self.meta_algo != 'NN':
             raise ValueError(f'meta algo {self.meta_algo} not supported for random search')
 
+        parameter_space = config("random_search_params")[0]    
         meta_algo = MLPRegressor(max_iter=200)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
         X_train, X_test = self._scale_data(X_train, X_test, save_model)
 
-        clf = RandomizedSearchCV(meta_algo, self.parameter_space, n_jobs=2, cv=2)
+        clf = RandomizedSearchCV(meta_algo, parameter_space, n_jobs=2, cv=2)
 
         clf.fit(X_train, y_train)
 
